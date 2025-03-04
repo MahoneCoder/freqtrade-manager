a1cb616604549ebed2dd5a56921a802cb03c4242
MahoneCoder <123318648+MahoneCoder@users.noreply.github.com>
2025-03-04 23:16:37 +0100

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -17,3 +17,13 @@ function Login() {
       alert('Login failed');
     }
   };
+
+    return (
+    <form onSubmit={handleLogin}>
+      <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+export default Login;
